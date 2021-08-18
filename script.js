7e5047efb1c9c86ed3189dc37720a5a13f9d80d2
Sulkifli Asmunandar <kflmattukz@gmail.com>
2021-08-18 16:26:20 +0800

    add id and completed field on todo form

--- script.js
+++ script.js
@@ -2,27 +2,23 @@ const todoInput = document.querySelector('.todo-input');
 const btnAdd = document.querySelector('.btn--add');
 const todoList = document.querySelector('.todolist');
 
-
 btnAdd.addEventListener('click' , function (e) {
   e.preventDefault()
-  
-  if (todoInput.value.match(/\s/g)) {
-    todoInput.value = '';
-  }
   
   if (todoInput.value === '') {
     console.log('TODO EMPTY');
     return false;
   }
   
-  todo.add(todoInput.value);
+  todo.add(30,todoInput.value , false);
 });
 
 todoList.addEventListener('click' , function (e) {
   const element  = e.target;
   const el = e.target.classList;
   if (el.contains('btn--done')) {
-    console.log(e.target);
+    // console.log(e.target);
+    todo.done(element);
   }
 
   if (el.contains('btn--del')) {
@@ -31,20 +27,30 @@ todoList.addEventListener('click' , function (e) {
 });
 
 const todo = {
-  add : function (todo) {
-    todoList.insertAdjacentHTML('afterbegin' , templateTodo(todo));
+  add : function (id,text,completed) {
+    todoList.insertAdjacentHTML('afterbegin' , templateTodo(id , text , completed));
     todoInput.value = '';
     todoInput.focus();
   },
   del : function (el) {
     el.parentElement.remove();
+  },
+  done : function (el) {
+    el.nextElementSibling.classList.toggle('completed');
+  },
+  load : function (todos) {
+    todos.map(item => {
+      this.add(item);
+    });
+    console.log('hello');
   }
 }
 
-function templateTodo(todo) {
-  return `<div class="todo">        
+function templateTodo(id, text, completed) {
+
+  return `<div class="todo" id="${ id }">        
             <button class="btn btn--done">Done</button>
-            <div class="todo-name">${ todo }</div>
+            <div class="todo-name ${ completed ? 'completed' : '' }">${ text }</div>
             <button class="btn btn--del">Delete</button>
           </div>`;
 }
