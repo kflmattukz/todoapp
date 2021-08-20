6a7eaf928520e66634e1423b1a4e6108e121bffc
kflmattukz <kflmattukz@gmail.com>
2021-08-20 19:51:34 +0800

    update : save the todo item to localStorage

--- js/app.js
+++ js/app.js
@@ -2,6 +2,19 @@ const todoInput = document.querySelector('.todo-input');
 const todoList = document.querySelector('.todo-list');
 const btnAdd = document.querySelector('.btn-add');
 
+let todos = [];
+
+//load data from localStorage
+
+if (localStorage.getItem('todos') === null) {
+  localStorage.setItem('todos' , JSON.stringify(todos));
+} else {
+  todos = JSON.parse(localStorage.getItem('todos'));
+  todos.map(todo => {
+    todoList.insertAdjacentHTML('beforeend' , templateTodo(todo));
+  });
+}
+
 btnAdd.addEventListener('click' , function(e){
   e.preventDefault();
 
@@ -45,7 +58,14 @@ todoList.addEventListener('keypress' , function (e){
 
 const todo = {
   add : function (todo) {
-    todoList.insertAdjacentHTML('beforeend' , templateTodo(todo));
+    const newTodo = {
+      id : todos.length+1,
+      text : todoInput.value,
+      completed : false
+    };
+    todos.push(newTodo);
+    localStorage.setItem('todos' , JSON.stringify(todos));
+    todoList.insertAdjacentHTML('beforeend' , templateTodo(newTodo));
     todoInput.value = '';
     todoInput.focus();
   },
@@ -76,15 +96,16 @@ const todo = {
   }
 }
 
-function templateTodo(todo) {
-  return `<div class="todo bg-white border border-blue-400 flex items-center py-2 px-1 rounded-md">
+
+function templateTodo({id, text, completed} = todo) {
+  return `<div class="todo bg-white border border-blue-400 flex items-center py-2 px-1 rounded-md" id="${ id }">
             <button class="complete text-green-600 hover:text-green-500">
               <svg xmlns="http://www.w3.org/2000/svg" class="h-7 w-7" fill="none" viewBox="0 0 24 24" stroke="currentColor">
                 <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M9 12l2 2 4-4m6 2a9 9 0 11-18 0 9 9 0 0118 0z" />
               </svg>
             </button>
-            <div class="edit todo-text flex-auto pl-3 text-gray-600">${ todo }</div>
-            <input type="text" class="hidden update flex-auto outline-none pl-3 text-gray-500" value="${ todo }">
+            <div class="edit todo-text flex-auto pl-3 text-gray-600 ${ completed ? 'line-through' : '' }">${ text }</div>
+            <input type="text" class="hidden update flex-auto outline-none pl-3 text-gray-500" value="${ text }">
             <button class="delete text-red-600 hover:text-red-500">
               <svg xmlns="http://www.w3.org/2000/svg" class="h-7 w-7" fill="none" viewBox="0 0 24 24" stroke="currentColor">
                 <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M10 14l2-2m0 0l2-2m-2 2l-2-2m2 2l2 2m7-2a9 9 0 11-18 0 9 9 0 0118 0z" />
