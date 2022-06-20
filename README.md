9fa1198b4d8744e57bbe3c3a8446b342fa4e77e4
Sulkifli Asmunandar <kflmattukz@gmail.com>
2022-06-21 03:05:19 +0800

    refactor and update README

--- README.md
+++ README.md
@@ -1,4 +1,6 @@
 # Todoapp
+A Simple app to manage your todo
+i build this app as i learning javascript
 ## manage your todos
 
 try this on : https://kflmattukz.github.io/todoapp/
--- js/app.js
+++ js/app.js
@@ -3,7 +3,6 @@ const todoAdd = document.querySelector(".todo-add");
 const todoList = document.querySelector(".todo-list");
 
 const todos = [];
-
 //localStorage function
 const data = {
   getId: function (id) {
